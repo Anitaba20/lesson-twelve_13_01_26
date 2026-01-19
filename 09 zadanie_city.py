faa7667749092023d369abcc7607a2dbc883cba3
anita <anita.bacova@gmail.com>
2026-01-19 23:27:30 +0100

    zadanie city

---
+++ 09 zadanie_city.py
@@ -0,0 +1,40 @@
+class Mesto:
+
+    def __init__(self, nazov_mesta, region, krajina, pocet_obyvatelov, psc, predvolba):
+        self.nazov_mesta = nazov_mesta
+        self.region = region
+        self.krajina = krajina
+        self.pocet_obyvatelov = pocet_obyvatelov
+        self.psc = psc
+        self.predvolba = predvolba
+
+    def adresa(self):
+        print(
+        self.nazov_mesta,
+        self.region,
+        self.krajina,
+        "PSČ",
+        self.psc,
+        "predvoľba",
+        self.predvolba)
+
+
+bratislava = Mesto(
+        "Bratislava",
+        "Bratislavský kraj",
+        "Slovensko",
+        500000,
+        "811 01",
+        "02")
+
+trencin = Mesto(
+        "Trenčín",
+        "Trenčiansky kraj",
+        "Slovensko",
+        30000,
+        "911 01",
+        "032")
+
+bratislava.adresa()
+
+trencin.adresa()
