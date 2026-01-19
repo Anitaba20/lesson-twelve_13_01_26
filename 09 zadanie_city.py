44f417f5ee6f96de61b5eeff59a2536971fb4425
anita <anita.bacova@gmail.com>
2026-01-19 23:35:46 +0100

    zadanie city

--- 09 zadanie_city.py
+++ 09 zadanie_city.py
@@ -1,5 +1,46 @@
-class Mesto:
+# class Mesto:
+#
+#     def __init__(self, nazov_mesta, region, krajina, pocet_obyvatelov, psc, predvolba):
+#         self.nazov_mesta = nazov_mesta
+#         self.region = region
+#         self.krajina = krajina
+#         self.pocet_obyvatelov = pocet_obyvatelov
+#         self.psc = psc
+#         self.predvolba = predvolba
+#
+#     def adresa(self):
+#         print(
+#         self.nazov_mesta,
+#         self.region,
+#         self.krajina,
+#         "PSČ",
+#         self.psc,
+#         "predvoľba",
+#         self.predvolba)
+#
+#
+# bratislava = Mesto(
+#         "Bratislava",
+#         "Bratislavský kraj",
+#         "Slovensko",
+#         500000,
+#         "811 01",
+#         "02")
+#
+# trencin = Mesto(
+#         "Trenčín",
+#         "Trenčiansky kraj",
+#         "Slovensko",
+#         30000,
+#         "911 01",
+#         "032")
+#
+# bratislava.adresa()
+#
+# trencin.adresa()
 
+
+class Mesto:
     def __init__(self, nazov_mesta, region, krajina, pocet_obyvatelov, psc, predvolba):
         self.nazov_mesta = nazov_mesta
         self.region = region
@@ -9,32 +50,11 @@ class Mesto:
         self.predvolba = predvolba
 
     def adresa(self):
-        print(
-        self.nazov_mesta,
-        self.region,
-        self.krajina,
-        "PSČ",
-        self.psc,
-        "predvoľba",
-        self.predvolba)
+        return f"{self.nazov_mesta}, {self.region}, {self.krajina}, PSČ {self.psc}, predvoľba {self.predvolba}"
 
 
-bratislava = Mesto(
-        "Bratislava",
-        "Bratislavský kraj",
-        "Slovensko",
-        500000,
-        "811 01",
-        "02")
+bratislava = Mesto("Bratislava", "Bratislavský kraj", "Slovensko", 500000, "811 01", "02")
+trencin = Mesto("Trenčín", "Trenčiansky kraj", "Slovensko", 30000, "911 01", "032")
 
-trencin = Mesto(
-        "Trenčín",
-        "Trenčiansky kraj",
-        "Slovensko",
-        30000,
-        "911 01",
-        "032")
-
-bratislava.adresa()
-
-trencin.adresa()
+print(bratislava.adresa())
+print(trencin.adresa())
